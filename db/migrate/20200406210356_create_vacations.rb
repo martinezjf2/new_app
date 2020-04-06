ae4d062c46c4f0a5cf7fbf0fe99dcd70dd36011b
github username <github email address>
2020-04-06 18:28:07 -0400

    Established Schema

--- db/migrate/20200406210356_create_vacations.rb
+++ db/migrate/20200406210356_create_vacations.rb
@@ -2,10 +2,10 @@ class CreateVacations < ActiveRecord::Migration
   def change
     create_table :vacations do |t|
       t.string :description
-      t.____  :start_date
-      t.____  :end_date
+      t.string :start_date
+      t.string :end_date
       t.string :resort_name
-      t.integer :user.id
+      t.integer :user_id
     end
   end
 end
--- db/migrate/20200406210404_create_shows.rb
+++ db/migrate/20200406210404_create_shows.rb
@@ -1,9 +1,10 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    creåte_table :shows do |t|
-      t._____ :start_time
-      t.____ :end_time
+    create_table :shows do |t|
+      t.string :start_time
+      t.string :end_time
       t.string :location
       t.integer :vacation_id
+    end
   end
 end
