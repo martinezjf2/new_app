3a209f246d93618bb3675bad9696e9b3f6d47bb8
Jeffrey Martinez <martinezjf2@gmail.com>
2020-04-06 22:25:45 -0400

    Add notes about sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #keep track of these sessions through the server, browser will keep a session saved through a cookie,(sessions are stored in a server, cookies are stored in the browser which keeps track of the specific session we are dealing with.) Session will stay the same until we log out the user. Also keeps our user logged in, and keeps it within the sessions hash.
+    set :session_secret, "password"
   end
 
   get "/" do
