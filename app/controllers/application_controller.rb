3df71b12630b337d303289bc7cd102d986fb5c01
github username <github email address>
2020-04-07 19:04:25 -0400

    Add user_controller get and post requests, implemented forms in erb files, and added the sinatra-flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions #keep track of these sessions through the server, browser will keep a session saved through a cookie,(sessions are stored in a server, cookies are stored in the browser which keeps track of the specific session we are dealing with.) Session will stay the same until we log out the user. Also keeps our user logged in, and keeps it within the sessions hash.
     set :session_secret, "password"
+    register Sinatra::Flash
   end
 
   get "/" do
@@ -16,5 +17,6 @@ class ApplicationController < Sinatra::Base
   get "/2" do
     erb :welcome1
   end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,49 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'users/sign_up'
+    end
+
+    get '/login' do
+        erb :'users/login'
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:message] = "Successul"
+            redirect to "/users/#{user.id}"
+        else
+            redirect to '/signup'
+        end
+    end
+
+
+    post '/signup' do
+        user = User.create(params)
+        if user.valid?
+            session[:user_id] = user.id
+        #what is the difference between re-directing and rendering?
+            redirect to "/users/#{user.id}"
+        else
+            if user.errors.messages[:username]
+                flash[:message] = "Username Invalid, Please try again"
+            elsif user.errors.messages[:password]
+                flash[:message] = "Password Invalid, Please try again"
+            #flash[:message] = "Input Invalid, Please try again"
+            redirect '/signup' #or maybe an error page that has 2 a href links to either signup or login
+        end
+    end
+
+    get '/users/:id' do
+        if User.find_by(id: params[:id])
+            @user = User.find_by(id: params[:id])
+        else
+            redirect to '/'
+        end
+        erb :'users/show'
+    end
+
 
 end
