088990726834593bd5f6a97342850368d1e16f16
Jeffrey Martinez <martinezjf2@gmail.com>
2020-04-07 19:04:25 -0400

    Add user_controller get and post requests, implemented forms in erb files, and added the sinatra-flash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
     has_many :vacations
         
-        has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    
+    has_secure_password
 
 end
