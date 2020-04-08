abd10b63793391da8cec401a7945837dd6c16236
Jeffrey Martinez <martinezjf2@gmail.com>
2020-04-08 16:23:06 -0400

    Fixed Flash error messages in post '/signup' request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,16 @@ class UsersController < ApplicationController
 
     post '/login' do
         user = User.find_by(username: params[:username])
-        if user && user.authenticate(params[:password])
+        user_password = user.authenticate(params[:password])
+        if user && user_password
             session[:user_id] = user.id
             # flash[:message] = "Successul"
             redirect to "/users/#{user.id}"
         else
-            binding.pry
-            if user.errors.messages[:username]
+            # binding.pry
+            if user.invalid?
                 flash[:message] = "Username not found, Please try again"
-            elsif user.errors.messages[:password]
+            elsif user_password.invalid?
                 flash[:message] = "Invalid Password, Please try again"
             end
             redirect to '/signup'
@@ -34,9 +35,10 @@ class UsersController < ApplicationController
         else
             if user.errors.messages[:username]
                 flash[:message] = "Username Has Already Been Used, Please try again"
-            # elsif user.errors.messages[:password]
-            #     flash[:message] = "Password Invalid, Please try again" #should use this within the '/login' get request?
+            elsif user.errors.messages[:email]
+                flash[:message] = "Email Invalid, Please try again" #should use this within the '/login' get request?
             #flash[:message] = "Input Invalid, Please try again"
+            #.errors method can only be used when creating a new User
             end
             redirect '/signup' #or maybe an error page that has 2 a href links to either signup or login
 
