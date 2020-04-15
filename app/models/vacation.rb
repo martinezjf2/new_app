07c0fd6e748da5eb1a51699f65b80b9b2c6fa80d
Jeffrey Martinez <martinezjf2@gmail.com>
2020-04-15 19:19:41 -0400

    refactoring

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,4 +1,11 @@
 class Vacation < ActiveRecord::Base
     has_many :shows
     belongs_to :user
+
+    validates :vacation_name, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validates :resort_name, presence: true
+    validates :description, presence: true
+
 end
