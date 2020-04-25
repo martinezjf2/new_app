e4d09cd5a9cbc9bced41aacf394cf26572614a6e
Jeffrey Martinez <martinezjf2@gmail.com>
2020-04-25 01:01:28 -0400

    add new CSS styling within the login.erb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,18 +25,19 @@ class UsersController < ApplicationController
 
     post '/login' do
         user = User.find_by(username: params[:username])
-        user_password = user.authenticate(params[:password])
-        if user && user_password
+        @user_password = user.authenticate(params[:password])
+        if user && @user_password
             session[:user_id] = user.id
             # flash[:message] = "Successul"
             redirect to "/users/#{user.id}"
+            # if username is not valid, it gives me a NoMethodError
         else
             if !user
                 flash[:message] = "Username not found, Please try again"
-            elsif !user_password
+            elsif !@user_password
                 flash[:message] = "Invalid Password, Please try again"
             end
-            redirect to '/signup'
+            redirect to '/login'
         end
     end
 
